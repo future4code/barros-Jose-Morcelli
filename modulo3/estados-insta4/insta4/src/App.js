af87c6a944472f73d3f901bfddb8889f7f55c099
Jose Morcelli <josemorcelli1@gmail.com>
2022-07-05 21:28:19 -0300

    insta4 parte 2

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import Post from './components/Post/Post';
+import { PostContainer } from './style';
 import './style.css'
 
+const listaPost=[
+  { nomeUsuario:'Paulinha',
+  fotoUsuario:'https://picsum.photos/50/50',
+  fotoPost:'https://picsum.photos/200/150'},
+
+  {nomeUsuario:'Joseph',
+  fotoUsuario:'https://picsum.photos/60/50',
+  fotoPost:'https://picsum.photos/190/150'},
+
+  {nomeUsuario:'Alef',
+  fotoUsuario:'https://picsum.photos/40/50',
+  fotoPost:'https://picsum.photos/180/150'},
+]
 
 function App() {
-return(
-  <div className='MainContainer'>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-          
-          <Post
-            nomeUsuario={'Joseph'}
-            fotoUsuario={'https://picsum.photos/60/50'}
-            fotoPost={'https://picsum.photos/190/150'}
-          />
-          
-          <Post
-            nomeUsuario={'Alef'}
-            fotoUsuario={'https://picsum.photos/40/50'}
-            fotoPost={'https://picsum.photos/180/150'}
-          />
-  </div>
+
+  const arrayComponentes= listaPost.map((elemento,index)=>{
+    return <li key={index}><Post 
+    nomeUsuario={elemento.nomeUsuario} 
+    fotoUsuario={elemento.fotoUsuario} 
+    fotoPost={elemento.fotoPost}/></li>
+    
+  })
+
+  console.log(listaPost)
+
+  return(
+    <PostContainer>
+    <ul>{arrayComponentes}</ul>
+    </PostContainer>
+  )
+
   
-)
+  
+
 
 }
 
