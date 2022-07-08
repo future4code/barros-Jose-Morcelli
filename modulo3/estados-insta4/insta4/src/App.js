4cd234250eae94dd3308e77ddac47cd09a88f044
Jose Morcelli <josemorcelli1@gmail.com>
2022-07-07 21:41:05 -0300

    insta4 lista 2

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,36 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './components/Post/Post';
-import { PostContainer } from './style';
+import { MyForm, PostContainer } from './style';
 import './style.css'
 
-const listaPost=[
-  { nomeUsuario:'Paulinha',
-  fotoUsuario:'https://picsum.photos/50/50',
-  fotoPost:'https://picsum.photos/200/150'},
 
-  {nomeUsuario:'Joseph',
-  fotoUsuario:'https://picsum.photos/60/50',
-  fotoPost:'https://picsum.photos/190/150'},
-
-  {nomeUsuario:'Alef',
-  fotoUsuario:'https://picsum.photos/40/50',
-  fotoPost:'https://picsum.photos/180/150'},
-]
 
 function App() {
 
+  const [listaPost,setListaPost]= useState([
+    { nome:'Paulinha',
+    foto:'https://picsum.photos/50/50',
+    post:'https://picsum.photos/200/150'},
+  
+    {nome:'Joseph',
+    foto:'https://picsum.photos/60/50',
+    post:'https://picsum.photos/190/150'},
+  
+    {nome:'Alef',
+    foto:'https://picsum.photos/40/50',
+    post:'https://picsum.photos/180/150'},
+    
+  ])
+
+  const [name,setName]= useState('')
+  const [foto,setFoto]= useState('')
+  const [msg,setMsg]= useState('')
+
   const arrayComponentes= listaPost.map((elemento,index)=>{
     return <li key={index}><Post 
-    nomeUsuario={elemento.nomeUsuario} 
-    fotoUsuario={elemento.fotoUsuario} 
-    fotoPost={elemento.fotoPost}/></li>
+    nomeUsuario={elemento.nome} 
+    fotoUsuario={elemento.foto} 
+    fotoPost={elemento.post}/></li>
     
   })
+
+  const handleName= (e) =>{
+    e.preventDefault()
+    setName(e.target.value)
+  }
+
+  const handleFoto= (e) =>{
+    e.preventDefault()
+    setFoto(e.target.value)
+  }
+
+  const handleMsg= (e) =>{
+    e.preventDefault()
+    setMsg(e.target.value)
+  }
+
+  const handleClick= (e) =>{
+    e.preventDefault()
+    const novoPost = [...listaPost,{nome: name, foto:"https://picsum.photos/40/50", post:"https://picsum.photos/180/150"}]
+    setListaPost(novoPost)
+    setName('')
+    setFoto('')
+    setMsg('')
+  }
 
   console.log(listaPost)
 
   return(
     <PostContainer>
+    <MyForm><input type="text" name='name' value={name} placeholder='nome' onChange={handleName}></input>
+    <input type="url" value={foto} placeholder='foto do perfil' onChange={handleFoto}></input>
+    <input type="url" value={msg} placeholder='imagem do post' onChange={handleMsg}></input>
+    <button onClick={handleClick}>Adicionar</button></MyForm>
+
+    
+      
     <ul>{arrayComponentes}</ul>
     </PostContainer>
   )
