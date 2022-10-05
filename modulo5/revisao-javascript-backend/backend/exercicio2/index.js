939255198f28006467e2d8f518be7f5c17d8f984
jose <josemorcelli1@gmail.com>
2022-10-04 23:06:16 -0300

    iniciando revisao, exercicio 1 finalizado, iniciando exercicio 2, faltando exercicio 3 e 4 a serem feitos.

---
+++ modulo5/revisao-javascript-backend/backend/exercicio2/index.js
@@ -0,0 +1,12 @@
+console.log("exercício 2");
+
+const clientesFieis = [
+	{ id: 1, nome: "Fulano" },
+	{ id: 2, nome: "Ciclano" },
+	{ id: 3, nome: "Beltrano" },
+	{ id: 4, nome: "Fulana"}
+]
+
+const novoCliente = () => {
+    console.log (clientesFieis)
+}
