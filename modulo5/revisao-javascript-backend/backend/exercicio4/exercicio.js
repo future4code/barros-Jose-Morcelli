1f4d929ef7d031ba0d1efce2fa75bda68a7469ae
jose <josemorcelli1@gmail.com>
2022-10-07 19:42:31 -0300

    exercicio quatro finalizado, revisao concluida!

--- modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
+++ modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
@@ -1,1 +1,33 @@
 console.log("exercicio 4");
+
+const primeiraLista = [
+	{
+		nome: "Banana"
+	},
+	{
+		nome: "Laranja"
+	},
+    {
+		nome: "Ameixa"
+	}
+]
+
+const segundaLista= [
+	{
+		nome: "Laranja"
+	},
+	{
+		nome: "Cebola"
+	},
+    {
+		nome: "Carambola"
+	}
+]
+
+const terceiraLista = [...primeiraLista, ...segundaLista]
+
+let listas = terceiraLista.filter((a) => {
+	return !this[JSON.stringify(a)] && (this[JSON.stringify(a)] = true);
+}, Object.create(null))
+
+console.log(listas)
