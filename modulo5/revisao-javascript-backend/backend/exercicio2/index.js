783c6acf9fa2a1f118ab596d9f0f74216199b7fd
jose <josemorcelli1@gmail.com>
2022-10-06 23:32:40 -0300

    exercicio 2 e exercicio 3 feitos, faltando apenas o exercicio 4.

--- modulo5/revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio2/index.js
@@ -1,4 +1,4 @@
-console.log("exercício 2");
+console.log("exercício 2 - Cadastro de cliente");
 
 const clientesFieis = [
 	{ id: 1, nome: "Fulano" },
@@ -7,6 +7,45 @@ const clientesFieis = [
 	{ id: 4, nome: "Fulana"}
 ]
 
-const novoCliente = () => {
-    console.log (clientesFieis)
+const registrarCliente = (novoCliente) =>{
+    const checarId =() =>{
+			for(let cliente of clientesFieis){
+            if(cliente.id === novoCliente.id){
+			return true
+			}
+	}
 }
+
+if(checarId() === true){
+	console.log(`Parâmetro inválido ('Id ${novoCliente.id} já existe').`)
+} else if(typeof novoCliente.name === "string"){
+	clientesFieis.push(novoCliente)
+}
+}
+registrarCliente({ id: 5, name: "Morcelli"})
+
+
+console.log(clientesFieis)
+
+
+console.log("exercicio 2 - Geração de tabuada")
+
+const geracaoDeTabuada = (numero) => {
+
+	let multipicacao = 0
+	let tabuada = []
+
+	if(typeof numero !== "number" ){
+		console.log("Parâmetro inválido ('tem que ser um número')")
+	} else if(numero > 10){
+		console.log("Parâmetro inválido ('número tem que valer entre 1 e 10')")
+	} else {
+		for(let i = 0; i <=10; i++){
+			multipicacao = `${numero} * ${i} = ${numero * i}`
+			tabuada.push(multipicacao)
+		}
+		console.log(tabuada)
+	}
+}
+
+geracaoDeTabuada(3)
