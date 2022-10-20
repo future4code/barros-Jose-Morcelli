fcd3ef1d86e4f8dd7a3e951f8fef277cc6029bcd
jose <josemorcelli1@gmail.com>
2022-10-20 19:17:46 -0300

    exercicios feitos do 1 ao 5, faltando 6,7 e 8.

---
+++ modulo5/lista-typescript/src/exercicio4/index.ts
@@ -0,0 +1,31 @@
+console.log("Exercício 4");
+
+enum SETOR {
+ MARKETING = "marketing",
+ VENDAS = "vendas",
+ FINANCEIRO = "financeiro"
+}
+
+type colaboradores ={
+    nome: string,
+    salário: number,
+    setor: SETOR,
+    presencial: boolean
+}
+
+const funcionarios: colaboradores[] =[
+        { nome: "Marcos", salário: 2500, setor: SETOR.MARKETING, presencial: true },
+        { nome: "Maria" ,salário: 1500, setor: SETOR.VENDAS, presencial: false},
+        { nome: "Salete" ,salário: 2200, setor: SETOR.FINANCEIRO, presencial: true},
+        { nome: "João" ,salário: 2800, setor: SETOR.MARKETING, presencial: false},
+        { nome: "Josué" ,salário: 5500, setor: SETOR.FINANCEIRO, presencial: true},
+        { nome: "Natalia" ,salário: 4700, setor: SETOR.VENDAS , presencial: true},
+        { nome: "Paola" ,salário: 3500, setor: SETOR.MARKETING, presencial: true }
+    ]
+
+
+function escolherColaborador(funcionarios: colaboradores[]): colaboradores[] {
+return funcionarios.filter(item => item.setor === "marketing" && item.presencial === true)
+}
+
+console.table(escolherColaborador(funcionarios))
