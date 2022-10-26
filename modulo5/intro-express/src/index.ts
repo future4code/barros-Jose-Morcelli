767c9099e67c71a844f0fbc3de4df352bdcf8032
jose <josemorcelli1@gmail.com>
2022-10-26 13:32:08 -0300

    exercicios finalizados 4 ao 8

--- modulo5/intro-express/src/index.ts
+++ modulo5/intro-express/src/index.ts
@@ -8,13 +8,13 @@ app.use(express.json())
 app.use(cors())
 
 app.listen(3003, () => {
-    console.log("Server is running in http://localhost:3003");
+    console.log("Servidor Executando em http://localhost:3003");
 });
 
 //---------------------------------------Exercício 1----------------------------------------------------------------
 
 app.get("/", (req: Request, res: Response) => {          
-    res.send("Hello from Express")
+    res.status(200).send("Hello from Express")
 })
 
 //---------------------------------------Exercício 2----------------------------------------------------------------
@@ -69,4 +69,77 @@ const users :Usuarios[] = [
 
 //---------------------------------------Exercício 4----------------------------------------------------------------
 
+app.get("/users", (req: Request, res: Response) => {          
+    
+    res.status(200).send(users)
+})
 
+//---------------------------------------Exercício 5----------------------------------------------------------------
+
+type Post = {
+    id: number,
+    title: string,
+    body: string,
+    userId: number
+}
+
+//----------------------------------------Exercício 6----------------------------------------------------------------
+
+//R: Pelo que eu entendi seria melhor criar fora, que somente o id seria possivel criar dentro do array do usuario, 
+//o userId no post ja é pra gente verificar e vincular com um usuario, sendo assim nao haveria necessidade ser dentro do array.
+
+const posts: Post[] = [
+    {
+        id: 1,
+        title: "Hello",
+        body: "World",
+        userId: 1
+    },
+    {
+        id: 2,
+        title: "Hello",
+        body: "World",
+        userId: 2
+    },
+    {
+        id: 3,
+        title: "Hello",
+        body: "World",
+        userId: 3
+    },
+    {
+        id: 4,
+        title: "Hello",
+        body: "World",
+        userId: 4
+    },
+    {
+        id: 5,
+        title: "Hello",
+        body: "World",
+        userId: 5
+    },
+]
+
+//---------------------------------------Exercício 7----------------------------------------------------------------
+
+app.get('/users/posts', (req: Request, res: Response) => {
+  
+    res.status(200).send(posts)
+
+})
+
+//---------------------------------------Exercício 8----------------------------------------------------------------
+
+app.get('/users/posts/:userId', (req: Request, res: Response) => {
+    
+    const userId = req.params.userId;
+    const verificaPost = posts.find( post => post.userId === Number(userId));
+    const filtrarPost = posts.filter( post => post.userId === Number(userId)); 
+    if(!verificaPost){
+      return res.status(401).send("Id inexistente! Digite um Id valido!");
+    } else {
+      return res.status(201).send(filtrarPost);
+    }
+  
+  })
