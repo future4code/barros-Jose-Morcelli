bd67cb6d9b8a8d90cb6bc66b7d0696956a79671c
jose <josemorcelli1@gmail.com>
2022-11-21 22:20:07 -0300

    inicio de exercicios filtros-ordenacao-paginacao

---
+++ modulo5/filtros-ordenacao-paginacao/src/index.ts
@@ -0,0 +1,43 @@
+import express, {Request,Response} from "express"
+import connection from './dataBase/connection'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+/*export const getAllUsers = async(req: Request,res: Response): Promise<void> =>{
+    try {
+       const users = await selectAllUsers()
+ 
+       if(!users.length){
+          res.statusCode = 404
+          throw new Error("No recipes found")
+       }
+ 
+       res.status(200).send(users)
+       
+    } catch (error) {
+       console.log(error)
+       res.send(error.message || error.sqlMessage)
+    }
+ }
+
+ export default async function selectAllUsers():Promise<any> {
+    const result = await connection.raw(`
+       SELECT id, name, email, type
+       FROM aula48_exercicio;
+    `)
+ 
+    return result[0]
+ }*/
+
+ //-----------------------Exercício 1 --------------------------------
+
+ //a)R:
+
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta http://localhost:3003");
+});
